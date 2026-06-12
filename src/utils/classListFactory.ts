17f33b4f0be753ef58ab817cd58b61d6989f5173
stefon.simmons <stefon.simmons@datadoghq.com>
2026-06-12 16:38:08 -0400

    use BEM and css module stretegy

---
+++ src/utils/classListFactory.ts
@@ -0,0 +1,26 @@
+/**
+ * classListFactory
+ *
+ * Returns a `cl` helper that applies both a semantic BEM class and its
+ * corresponding hashed CSS module class simultaneously.
+ *
+ * Usage:
+ *   import styles from './MyComponent.module.css';
+ *   import { classListFactory } from '../../utils/classListFactory';
+ *
+ *   const cl = classListFactory(styles);
+ *
+ *   // Usage: In astro file:
+ *   <div class={cl('my-block', 'my-block--modifier')}>
+ */
+export function classListFactory(styles: Record<string, string>) {
+  return function cl(...classNames: string[]): string {
+    return classNames
+      .flatMap((name) => {
+        console.log(styles)
+        const hashed = styles[name];
+        return hashed ? [name, hashed] : [name];
+      })
+      .join(' ');
+  };
+}
